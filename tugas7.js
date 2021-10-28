a6b9ef5edcdac13e1c3a465e6a51803be125b888
ihlas_sul <ihlassmada@gmail.com>
2021-10-28 08:07:02 +0800

    tugas 7 javascript
    done

---
+++ tugas7.js
@@ -0,0 +1,9 @@
+let tinggiBadan = [168,165,170,180,184,160,155,167,171,175]
+console.log('perulanagan for')
+for(let i=0; i<tinggiBadan.length; i++){
+    console.log(tinggiBadan[i])
+}console.log('\n')
+console.log('perulangan for of')
+for(let x of tinggiBadan){
+    console.log(x)
+}
